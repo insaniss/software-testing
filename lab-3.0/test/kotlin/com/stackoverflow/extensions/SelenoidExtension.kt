6429f583e012a8c66b571cde328143a52ce6a608
Bobur Zakirov <bobur.octo@gmail.com>
2022-05-16 11:09:42 +0300

    Add property `baseUrl`

--- lab-3.0/test/kotlin/com/stackoverflow/extensions/SelenoidExtension.kt
+++ lab-3.0/test/kotlin/com/stackoverflow/extensions/SelenoidExtension.kt
@@ -13,6 +13,7 @@ class SelenoidExtension : BeforeAllCallback {
         props.load(this::class.java.classLoader.getResourceAsStream("selenoid.properties"))
 
         remote = this.props.getProperty("selenoid.remote")
+        baseUrl = this.props.getProperty("selenoid.base-url")
         browser = this.props.getProperty("selenoid.browser")
         browserVersion = this.props.getProperty("selenoid.browser-version")
 
